43e7c434787aafed7729b2a2eacf949b7b7b2c60
Renner McCreath <rennermccreath@gmail.com>
2025-11-23 20:34:36 -0500

    two pages

--- src/pages/Exp4.jsx
+++ src/pages/Exp4.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
+import may2026 from "../assets/may2026.jpg";
 
 export default function Exp4() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-white">
-      <h1 className="text-4xl font-bold mb-4">Experience 4</h1>
-      <p>This is the Exp4 page placeholder.</p>
+      <h2 className="text-4xl font-bold mb-4">Dust off the hiking boots...</h2>
+      <h2 className="text-4xl font-bold mb-4 text-center">West of Yonge Tour Guide/Uncle Tony reports for duty starting May 2026!</h2>
+      <motion.img
+          src={may2026}
+          alt="Logo"
+          className="w-5/8 max-w-sm md:max-w-md lg:max-w-lg h-auto object-contain"
+          initial={{ scale: 0.9, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.2, ease: "easeOut" }}
+        />
     </div>
+    
   );
 }
