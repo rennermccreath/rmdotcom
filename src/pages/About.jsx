470bed45728deb7a21b968ad56d3c24bfd57137d
Renner McCreath <rennermccreath@gmail.com>
2026-02-16 12:51:05 -0500

    spelling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -41,19 +41,16 @@ export default function About() {
       <div className="md:row-span-2 rounded-2xl p-0 sm:p-8 flex flex-col justify-center items-center text-start shadow-lg">
         {/* <h3 className="text-4xl font-semibold mb-3">Accredited. Experienced. Adaptable.</h3> */}
         <p className="text-xl opacity-90 mb-6 max-w-lg">
-          Nice to meet you! My name is Renner McCreath - a customer success advisor, project manager and business analyst with over ten years of experience across several sectors, indcluding education, finance, and technology.
-        </p>
+          Nice to meet you! My name is Renner McCreath - a customer success advisor, project manager and business analyst with over ten years of experience across several sectors, including education, finance, and technology.        </p>
         <p className="text-xl opacity-90 mb-6 max-w-lg">
-          I am currently a Student Success Advisor with Boreal Education - an EdTech start-up empowering Canadian post-secondary insitutions to grow and develop distance, hybrid and online education programs. In my role, I work directly with both undergraduate and post-grad student cohorts to achieve their academic and personal goals.
-        </p>
+          I am currently a Student Success Advisor with Boreal Education - an EdTech start-up empowering Canadian post-secondary institutions to grow and develop distance, hybrid and online education programs. In my role, I work directly with undergraduate and postgraduate student cohorts to help them achieve their academic and personal goals.        </p>
         {/* <p className="text-xl opacity-90 mb-6 max-w-lg">
           I am currently a Management Consultant with my firm, Viscount Pierce. In my role, I help enterprise clients in tech and finance define strategic priorities, modernize operations, and execute transformation initiatives. My approach combines structured frameworks, collaborative problem-solving, and a focus on building scalable solutions for long-term growth.
         </p> */}
         <p className="text-xl opacity-90 mb-6 max-w-lg">
-          Prior to Boreal, I helped scale operations and customer success at Fruit Suite - a food service start-up delivering B2B subscriptions of fruit, snacks, and beverages across the GTA. Through a transformation of the Fruit Suite narrative and a re-design of key customer success principles, Fruit Suite enjoyed a significant uptick in trial program conversions, several banner months, and the company's most successful year to date in 2025. Equally as important, Fruit Suite made significant contributions to combat food inequality, donating close to half a million pieces of fruit in the past year to shelters, food banks, hospitals, and local schools.
-        </p>
+          Prior to Boreal, I helped scale operations and customer success at Fruit Suite, a food service startup delivering B2B subscriptions of fruit, snacks, and beverages across the GTA. Through a transformation of the Fruit Suite narrative and a redesign of key customer success principles, Fruit Suite saw a significant uptick in trial program conversions, several banner months, and its most successful year to date in 2025. Equally important, Fruit Suite made significant contributions to combat food inequality, donating nearly half a million pieces of fruit over the past year to shelters, food banks, hospitals, and local schools.        </p>
         <p className="text-xl opacity-90 mb-6 max-w-lg">
-          As Senior Business Analyst and Project Manager of CI Global Asset Management's National Accounts team, I led sales and marketing projects to approve award-winning investment solutions at Canada's largest Mutual Fund dealers. Managing multiple teams of 2-10 on projects spanning up to six months - and relying on my strengths of communication, stakeholder management, and project scaling - I assisted in generating over $100 million in annual gross sales since 2019.
+          As a Senior Business Analyst and Project Manager for CI Global Asset Management's National Accounts team, I led sales and marketing projects to secure approval for award-winning investment solutions with Canada's largest Mutual Fund dealers. Managing multiple teams of 2-10 on projects spanning up to six months, and relying on my strengths in communication, stakeholder management, and project scaling, I have helped generate over $100 million in annual gross sales since 2019.
         </p>
         <p className="text-xl opacity-90 mb-6 max-w-lg">
           For a more in-depth look at my career, please navigate using the arrows on either side of the page, or swiping on mobile.
