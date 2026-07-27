d849f4aef982ee8bff0cafc16c1fadb596ac0b6a
Renner McCreath <rennermccreath@gmail.com>
2026-07-27 12:22:02 -0400

    tweak

--- src/pages/CaribouQ320261.jsx
+++ src/pages/CaribouQ320261.jsx
@@ -52,7 +52,7 @@ export default function CaribouQ320261() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}
             >
-              Candidate Presentation - Senior Customer Success Associate, Strategic Markets
+              Candidate Presentation - Senior Customer Success Associate
             </motion.p>
             
 
